bb58c978ab9e004246dee7fd5afe02c05413a06f
joseilton <joseiltoniurd@gmail.com>
2022-02-25 00:21:00 -0300

    setenca ,blocos de codigos e comentarios (introducao)

---
+++ FUNDAMENTOS/bloco 1.js
@@ -0,0 +1,15 @@
+// 5 setencas e 2 blocos codigos
+{
+  console.log("passo #01");
+  console.log("passo #02");
+  console.log("passo #03");
+}
+  
+
+
+ {
+  console.log("passo #04");
+  console.log("passo #05");
+ } 
+ 
+ console.log.apply("fim!")
