6a03114cf8bb8f0471ce8e17607a555156c39a46
joseilton <joseiltoniurd@gmail.com>
2022-02-28 12:31:37 -0300

    numeros,tipagemFraca,variaveis e constantes

---
+++ FUNDAMENTOS/variaveis.Constantes.js
@@ -0,0 +1,19 @@
+var a = 3
+let b = 4
+
+var a =30
+b = 50
+
+console.log (a,b)
+
+a = 300
+b = 500
+
+console.log (a,b)
+
+const c = 5
+// c = 50
+console.log(c)
+
+console.log(a,b,c)
+console.log(a + b + c)
