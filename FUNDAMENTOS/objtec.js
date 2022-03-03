5bd7ea31e3b787c42b294e729c795a5a70bc752a
joseilton <joseiltoniurd@gmail.com>
2022-03-02 23:35:08 -0300

    Array,Boolean, Objetc

---
+++ FUNDAMENTOS/objtec.js
@@ -0,0 +1,14 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra mega'
+prod1.preco = 4899,90
+prod1['Desconto Legal'] = 0.4 // evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: "arroz Tio jorge",
+    preco: 21.90,
+    peso: 5
+}
+console.log(prod2)
+
