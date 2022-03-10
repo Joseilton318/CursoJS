901f8f9cb97cbf43ae98cbb686916590f2a27746
joseilton <joseiltoniurd@gmail.com>
2022-03-09 23:52:50 -0300

    logicos,unarios ,ternarios ,experimentos e comentarios

---
+++ FUNDAMENTOS/comentarios2.js
@@ -0,0 +1,15 @@
+// v e v -> v
+// v e f -> f
+// v e ? -> f
+
+// v ou ? -> v
+// v ou v -> v
+// f ou f -> f
+
+// v xor v -> f
+// v xor f -> v
+// f xor v -> v
+// f xor f -> f
+
+// !v ->f
+// !f ->v
