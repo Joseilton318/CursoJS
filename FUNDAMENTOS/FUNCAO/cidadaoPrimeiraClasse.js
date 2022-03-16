bf0e91e403235406cf8d557513705501d1266a83
joseilton <joseiltoniurd@gmail.com>
2022-03-15 22:53:40 -0300

    primeira classe e parametros , retornos

--- FUNDAMENTOS/FUNCAO/cidadaoPrimeiraClasse.js
+++ FUNDAMENTOS/FUNCAO/cidadaoPrimeiraClasse.js
@@ -5,4 +5,29 @@
 function fun1() {}
 
 // Armanazer em uma variavel
-const fun2  functi
+const fun2 = function () {}
+
+//Armazenar em um array
+const array = [function(a,b) {return a + b}, fun1, fun2]
+console.log(array[0](2, 3))
+
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () {return 'Opa'}
+console.log(obj.falar())
+
+//Passar função como param
+function run(fun)  {
+    fun()
+}
+
+run(function () {console.log('Executando') })
+
+//Uma função pode retornar/conter uma funcao
+function soma(a, b) {
+    return function (c) {
+        console.log(a + b + c)
+    }
+}
+
+soma(2,3)(4)
