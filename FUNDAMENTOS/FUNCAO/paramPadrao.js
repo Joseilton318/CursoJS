9e201b955d1be8496c629eb1cccee1676617ab29
joseilton <joseiltoniurd@gmail.com>
2022-03-17 23:20:38 -0300

    paramPadraoeVariaveisEThisBind

--- FUNDAMENTOS/FUNCAO/paramPadrao.js
+++ FUNDAMENTOS/FUNCAO/paramPadrao.js
@@ -16,11 +16,11 @@ function soma2(a, b, c) {
     return a + b + c
 }
 
-console.log(soma2(2), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))  
+console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))  
 
 //valor padrao do es2015
-function soma3(a = 1, b = 1, c = 1) {
-    return a + b + c
-}
+//function soma3(a = 1, b = 1, c = 1) {
+   // return a + b + c
+//}
 
-console.log(soma3(), soma3(3), soma(1, 2, 3), soma3(0, 0, 0))
+//console.log(soma3(), soma3(3), soma(1, 2, 3), soma3(0, 0, 0))
--- FUNDAMENTOS/FUNCAO/paramsVariaveis.js
+++ FUNDAMENTOS/FUNCAO/paramsVariaveis.js
@@ -3,11 +3,11 @@ function soma() {
     for (i in arguments) {
         soma += arguments [i]
     }
-    return soma a + b + c
+    return soma 
 }
 
 console.log(soma())
 console.log(soma(1))
 console.log(soma(1.1, 2.2, 3.3))
 console.log(soma(1.1, 2.2, "Teste"))
-console.log(soma('a', 'b', ' c'))
+console.log(soma('a', 'b', 'c'))
