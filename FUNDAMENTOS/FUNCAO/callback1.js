6889d9b6386d5e9d2b0547978f741fe6e8931662
joseilton <joseiltoniurd@gmail.com>
2022-03-21 22:50:30 -0300

    arrowfunction,funcoes anonimas e callbacks

---
+++ FUNDAMENTOS/FUNCAO/callback1.js
@@ -0,0 +1,7 @@
+const fabricantes = ["Mercedes", "Audi", "Bmw"]
+
+function imprimir(nome, indice) {
+    console.log(`${indice + 1}. $(nome)`)
+}
+
+fabricantes.forEach(imprimir)
---
+++ FUNDAMENTOS/FUNCAO/callback3.js
@@ -0,0 +1,4 @@
+//Exemplo de callback no Browser
+document.getElementsByTagNane('body') [0].onclick = function (e) {
+    console.log( 'O evento ocorreu')
+}
