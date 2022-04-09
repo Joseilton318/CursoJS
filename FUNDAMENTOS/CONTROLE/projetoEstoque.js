004b3b46839b1807761a431d4914a2ffc292807d
joseilton <joseiltoniurd@gmail.com>
2022-04-08 22:49:16 -0300

    projeto1

---
+++ FUNDAMENTOS/CONTROLE/projetoEstoque.js
@@ -0,0 +1,24 @@
+const estoque = {
+    arroz: 35,
+    feijão: 42,
+    oleo:43,
+    sardinha:68,
+    macarrão:42
+
+};
+
+function fluxoEstoque(){
+    const totalEstoque =
+    estoque.arroz - 
+    estoque.feijao - 
+    estoque.macarrão -
+    estoque.oleo - 
+    estoque.sardinha - ;
+
+    return totalEstoque;
+
+}
+
+function arroz(){
+    estoque.arroz = () -
+}
