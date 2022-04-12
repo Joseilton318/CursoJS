f67b6a6f5817563fad128fd8c74b1547ee469a93
joseilton <joseiltoniurd@gmail.com>
2022-04-11 22:49:38 -0300

    continuacaoprojeto

--- FUNDAMENTOS/CONTROLE/projetoEstoque.js
+++ FUNDAMENTOS/CONTROLE/projetoEstoque.js
@@ -1,24 +1,49 @@
 const estoque = {
-    arroz: 35,
-    feijão: 42,
-    oleo:43,
-    sardinha:68,
-    macarrão:42
+    arroz:{quantidade: 35},
+    feijao:{quantidade: 42},
+    oleo:{quantidade: 36},
+    sardinha:{quantidade: 48},
+    macarrao:{quantidade: 65},
 
 };
 
-function fluxoEstoque(){
-    const totalEstoque =
-    estoque.arroz - 
-    estoque.feijao - 
-    estoque.macarrão -
-    estoque.oleo - 
-    estoque.sardinha - ;
+    const totalEstoque = {
+    estoque.quantidade.arroz +
+    estoque.quantidade.feijao + 
+    estoque.quantidade.oleo +
+    estoque.quantidade.sardinha +
+    estoque.quantidade.macarrao 
+}
 
-    return totalEstoque;
+function vendaArroz() {
+    estoque.arroz.quantidade = estoque.arroz.quantidade - quantidade;
+}
+
+function vendaFeijao() {
+    estoque.feijao.quantidade = estoque.feijao.quantidade - quantidade;
+}
+
+function vendaOleo() {
+    estoque.oleo.quantidade = estoque.oleo.quantidade - quantidade;
+}
+
+function vendaSardinha() {
+    estoque.sardinha.quantidade = estoque.sardinha.quantidade - quantidade;
+}
+    
+function vendaMacarrao() {
+    estoque.macarrao.quantidade = estoque.macarrao.quantidade - quantidade
 
 }
 
-function arroz(){
-    estoque.arroz = () -
-}
+vendaArroz(15)
+vendaFeijao(16)
+vendaOleo(7)
+vendaSardinha(8)
+vendaMacarrao(19)
+
+console.log(vendaArroz,vendaFeijao, vendaOleo, vendaSardinha, vendaMacarrao)
+console.log(totalEstoque)
+
+
+
