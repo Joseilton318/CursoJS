c39bebcbd18f6b7e8c77a181a3708b4d71ddb2aa
joseilton <joseiltoniurd@gmail.com>
2022-04-12 22:29:07 -0300

    projeto inicial

--- FUNDAMENTOS/CONTROLE/projetoEstoque.js
+++ FUNDAMENTOS/CONTROLE/projetoEstoque.js
@@ -1,37 +1,39 @@
 const estoque = {
-    arroz:{quantidade: 35},
-    feijao:{quantidade: 42},
-    oleo:{quantidade: 36},
-    sardinha:{quantidade: 48},
-    macarrao:{quantidade: 65},
+    arroz: {quantidade: 35},
+    feijao: {quantidade: 42},
+    oleo: {quantidade: 36},
+    sardinha: {quantidade: 48},
+    macarrao: {quantidade: 65},
 
 };
 
-    const totalEstoque = {
-    estoque.quantidade.arroz +
-    estoque.quantidade.feijao + 
-    estoque.quantidade.oleo +
-    estoque.quantidade.sardinha +
-    estoque.quantidade.macarrao 
-}
+    function totalEstoque(quantidade){
+        return
+    totalEstoque.arroz.quantidade +
+    totalEstoque.feijao.quantidade + 
+    totalEstoque.oleo.quantidade +
+    totalEstoque.sardinha.quantidade +
+    totalEstoque.macarrao.quantidade 
+    
+};
 
-function vendaArroz() {
+function vendaArroz(quantidade) {
     estoque.arroz.quantidade = estoque.arroz.quantidade - quantidade;
 }
 
-function vendaFeijao() {
+function vendaFeijao(quantidade) {
     estoque.feijao.quantidade = estoque.feijao.quantidade - quantidade;
 }
 
-function vendaOleo() {
+function vendaOleo(quantidade) {
     estoque.oleo.quantidade = estoque.oleo.quantidade - quantidade;
 }
 
-function vendaSardinha() {
+function vendaSardinha(quantidade) {
     estoque.sardinha.quantidade = estoque.sardinha.quantidade - quantidade;
 }
     
-function vendaMacarrao() {
+function vendaMacarrao(quantidade) {
     estoque.macarrao.quantidade = estoque.macarrao.quantidade - quantidade
 
 }
@@ -42,8 +44,13 @@ vendaOleo(7)
 vendaSardinha(8)
 vendaMacarrao(19)
 
-console.log(vendaArroz,vendaFeijao, vendaOleo, vendaSardinha, vendaMacarrao)
-console.log(totalEstoque)
+console.log(estoque.arroz, estoque.feijao, estoque.oleo, estoque.sardinha, estoque.arroz)
+console.log('ARROZ',estoque.arroz)
+console.log('FEIJAO', estoque.feijao)
+console.log('OLEO',estoque.oleo)
+console.log('SARDINHA',estoque.sardinha)
+console.log('OLEO',estoque.oleo)
+console.log(estoque)
 
 
 
